e60a6e16652cc3e29780af02eb2f660905ca64f3
subbuwu <nsubbu2004@gmail.com>
2023-09-12 13:58:56 +0530

    UI Changes

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,21 +1,15 @@
-import PropTypes from 'prop-types';
-import './styles.css';
+import './styles.css'
 
-const Card = ({ name, desc }) => {
+const Card = ({name,desc,Image}) => {
   return (
-    <div className="flex flex-col w-[300px] h-[200px] bg-[#FFF] p-6 card my-6">
-      <div className="mx-auto">
-        <div className="bg-gray-500 rounded-[50%] mb-2 w-12 h-12 mx-auto text-center" />
-        <p className="text-center font-medium my">{name}</p>
-        <p className="text-center font-light">{desc}</p>
-      </div>
+    <div className='flex flex-col w-[300px] h-[200px] bg-[#FFF] p-6 card my-6'>
+        <div className='mx-auto'>
+          <img src={Image} className='team__card rounded-[50%] mb-2 w-20 h-20 mx-auto text-center'/>
+          <p className='text-center font-medium my'>{name}</p>
+          <p className='text-center font-light'>{desc}</p>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-Card.propTypes = {
-  name: PropTypes.string.isRequired,
-  desc: PropTypes.string.isRequired,
-};
-
-export default Card;
+export default Card
