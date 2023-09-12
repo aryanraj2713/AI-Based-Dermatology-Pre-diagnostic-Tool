e9e6fa25351af7f4f98b5057cdfbcf7f6d663929
Subramanian Narayanan <101008478+subbuwu@users.noreply.github.com>
2023-09-12 14:04:07 +0530

    UI Changes (#13)
    * Form and Responsiveness (#7)
    
    * fix model.json (#1)
    
    * update model.json
    
    * remove vercel.json
    
    * remove git lfs (#2)
    
    * fix model weights (#3)
    
    * remove git lfs
    
    * fix model weights
    
    * fix:deployment_issue
    
    * bug:deploymentbug_fix
    
    ---------
    
    Co-authored-by: Jatin <main.jatink@gmail.com>
    Co-authored-by: Aryan Raj <aryanraj2713@gmail.com>
    
    * UI Changes
    
    ---------
    
    Co-authored-by: Jatin <main.jatink@gmail.com>
    Co-authored-by: Aryan Raj <aryanraj2713@gmail.com>

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -1,26 +1,32 @@
 import Card from './Card';
 import './styles.css';
+import Image1 from '../assets/JatinImg.jpg';
+import Image2 from '../assets/AryanImg.jpg';
+import Image3 from '../assets/MathangyImg.jpg';
+import Image4 from '../assets/RavikulImg.jpg';
+import Image5 from '../assets/AakashImg.png';
+import Image6 from '../assets/SubbuImg.jpg';
 
 const Team = () => {
   return (
     <>
-      <div className="team__header w-full flex justify-center items-center h-[100px] mt-32">
-        <h1 className="team-title px-16 text-xl py-1 bg-white shadow-md rounded-full">
-          <span>Team</span>
+    <div className='team__header w-full flex justify-center items-center h-[100px] mt-32'>
+        <h1 class="team-title px-16 text-xl py-1 bg-white shadow-md rounded-full">
+            <span>Team</span>
         </h1>
-      </div>
-      <div className="w-[100%]">
-        <div className="mx-auto w-[70%] flex flex-row flex-wrap gap-10 my-6 justify-center items-center">
-          <Card name="Aryan" desc="AI/ML Dev" />
-          <Card name="Mathangy" desc="AI/ML Dev" />
-          <Card name="Jatin" desc="Full Stack Dev" />
-          <Card name="Ravikul" desc="Full Stack Dev" />
-          <Card name="Aakash" desc="Full Stack Dev" />
-          <Card name="Subbu" desc="Junior Dev" />
+    </div>
+    <div className='w-[100%]'>
+        <div className="mx-auto w-[80%] max-xl:w-[90%] max-lg:w-[90%] flex flex-row flex-wrap gap-10 my-6 justify-center items-center">
+            <Card Image={Image2} name="Aryan" desc="AI/ML Developer"/>
+            <Card Image={Image3} name="Mathangy" desc="AI/ML Developer"/>
+            <Card Image={Image1} name="Jatin" desc="Full Stack Developer"/>
+            <Card Image={Image4} name="Ravikul" desc="Full Stack Developer"/>
+            <Card Image={Image5} name="Aakash" desc="Full Stack Developer"/>
+            <Card Image={Image6} name="Subramanian" desc="Frontend Developer"/>
         </div>
-      </div>
+    </div>
     </>
-  );
-};
+  )
+}
 
-export default Team;
+export default Team
